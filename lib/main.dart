f66e39cc7092072607f42a372436533255ddf589
Yayo <geramail28@gmail.com>
2021-02-16 23:05:56 +0800

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:basic_landing_webpage/src/my_webpage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Basic landing webpage",
+      home: MyWebpage(),
+    ),
+  );
+}
