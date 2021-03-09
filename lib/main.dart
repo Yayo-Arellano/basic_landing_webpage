2ff90c183329729f507b884f7e580d07ae6c28ef
Yayo <geramail28@gmail.com>
2021-03-09 23:13:53 +0800

    setPathUrlStrategy();

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:basic_landing_webpage/src/my_webpage.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(
     MaterialApp(
       title: "Basic landing webpage",
